17e714a750a0fc85a7fda105005881ec0a845be2
Chris Saunders <cs3@ualberta.ca>
2017-01-20 13:54:15 -0700

    Add files via upload

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/HappyMood.java
@@ -0,0 +1,11 @@
+package ca.ualberta.cs.lonelytwitter;
+
+/**
+ * Created by Kathy on 2017-01-20.
+ */
+
+public class HappyMood extends CurrentMood {
+    public String MyMood(){
+        return "Happy";
+    }
+}
